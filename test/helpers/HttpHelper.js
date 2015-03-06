e629cc45278ec7337537f56c1961493a676dadcf
Ralph Varjabedian <ralph.varjabedian@rakutenloyalty.com>
2015-03-06 15:14:26 +0200

    bugfixes/unit tests

---
+++ test/helpers/HttpHelper.js
@@ -0,0 +1,185 @@
+/**
+ * Created by Ralph Varjabedian on 10/23/13.
+ */
+
+function HttpHelper(hostname, port, defaultHeaders) {
+   this.IsHttpHelper = true;
+   this.hostname = hostname;
+   if (this.hostname.lastIndexOf("/") == this.hostname.length - 1) {
+      this.hostname = this.hostname.substring(0, this.hostname.length - 1);
+   }
+   this.port = port;
+   if (!this.port) {
+      this.port = 80;
+   }
+   this.defaultHeaders = defaultHeaders;
+   this.http = require("http");
+}
+
+HttpHelper.prototype = {
+   createGet: function(path) {
+      if (arguments.length > 1) {
+         for (var i = 1; i < arguments.length; i++) {
+            path += arguments[i];
+         }
+      }
+      var options = {
+         IsOptions: true,
+         ref: this,
+         hostname: this.hostname,
+         port: this.port,
+         path: path,
+         method: "GET",
+         headers: JSON.parse(JSON.stringify(this.defaultHeaders))
+      };
+      this.copyFunctionsToo(options);
+      return options;
+   },
+   get: function(params, cb) {
+      if (arguments.length == 1)
+         cb = params;
+      var self = this;
+      var options = self;
+      if (self.IsOptions) {
+         self = self.ref;
+      } else {
+         throw new Error("must use function createGet/createPost first");
+      }
+      if (params)
+         options.path += "?" + self.getGetParams(params);
+      self.http.get(options, function(res) {
+         res.fullBody = "";
+         res.setEncoding('utf8');
+         res.on("data", function(chunk) {
+            res.fullBody += chunk;
+         });
+         res.on("end", function() {
+            cb(null, res.fullBody, res);
+         });
+         res.on("error", function(e) {
+            cb(e);
+         })
+      });
+   },
+   getJson: function(params, cb) {
+      if (arguments.length == 1) {
+         cb = params;
+         params = null;
+      }
+      this.get(params, function(err, data, res) {
+         if (err) {
+            cb(err);
+         } else {
+            cb(err, JSON.parse(data), res);
+         }
+      })
+   },
+   createPost: function(path) {
+      var options = {
+         IsOptions: true,
+         ref: this,
+         hostname: this.hostname,
+         port: this.port,
+         path: path,
+         method: "POST",
+         headers: JSON.parse(JSON.stringify(this.defaultHeaders))
+      };
+      this.copyFunctionsToo(options);
+      return options;
+   },
+   getGetParams: function(paramsObject) {
+      var bodyStr = "";
+      for (var k in paramsObject) {
+         if (bodyStr != "")
+            bodyStr += "&";
+         if (typeof(paramsObject[k]) == "object")
+            paramsObject[k] = JSON.stringify(paramsObject[k]);
+         bodyStr += (k + "=" + encodeURI(paramsObject[k]));
+      }
+      return bodyStr;
+   },
+   post: function(postData, cb) {
+      var self = this;
+      var options = self;
+      if (self.IsOptions) {
+         self = self.ref;
+      } else {
+         throw new Error("must use function createPost first");
+      }
+      var postReq = self.http.request(options, function(res) {
+         res.fullBody = "";
+         res.setEncoding('utf8');
+         res.on("data", function(chunk) {
+            res.fullBody += chunk;
+         });
+         res.on("end", function() {
+            cb(null, res.fullBody, res);
+         });
+         res.on("error", function(e) {
+            cb(e);
+         })
+      });
+      if (typeof(postData) == "object")
+         postData = JSON.stringify(postData);
+      postReq.write(postData);
+      postReq.end();
+   },
+   postJson: function(postData, cb) {
+      this.post(postData, function(err, data, res) {
+         if (err) {
+            cb(err);
+         } else {
+            cb(err, JSON.parse(data), res);
+         }
+      })
+   },
+   modifyHeader: function(headerName, value) {
+      this.removeHeader(headerName);
+      return this.addHeader(headerName, value);
+   },
+   removeHeader: function(headerName) {
+      var self = this;
+      var options = self;
+      if (self.IsOptions) {
+         self = self.ref;
+      } else {
+         throw new Error("must use function createGet/createPost first");
+      }
+      delete options.headers[headerName];
+      return options;
+   },
+   addHeader: function(headerName, value) {
+      var self = this;
+      var options = self;
+      if (self.IsOptions) {
+         self = self.ref;
+      } else {
+         throw new Error("must use function createGet/createPost first");
+      }
+      options.headers[headerName] = value;
+      return options;
+   },
+   setMethod: function(method) {
+      var self = this;
+      var options = self;
+      if (self.IsOptions) {
+         self = self.ref;
+      } else {
+         throw new Error("must use function createGet/createPost first");
+      }
+      options.method = method;
+      return options;
+   },
+   /* private */
+   copyFunctionsToo: function(options) {
+      for (var e in HttpHelper.prototype) {
+         var func = HttpHelper.prototype[e];
+         if (typeof(func) == "function") {
+            options[e] = func;
+         }
+      }
+   },
+   __end: 0
+};
+
+module.exports = HttpHelper;
--- test/main.test.js
+++ test/main.test.js
@@ -1,18 +1,46 @@
 'use strict';
 
 var should = require('should');
+var assert = require('assert');
 var path = require('path');
 var port = 4444;
+
+var HttpHelper = require('./helpers/HttpHelper');
+var httpHelper = new HttpHelper("localhost", port, {
+  'accept': "application/json",
+  'content-type': "application/json",
+  'origin': 'unit-tests'
+});
 
 describe('testing nodetastic', function() {
   this.timeout(0);
 
-  it('testing require and hook', function(done) {
+  it('testing start server', function(done) {
     var nodetastic = require("../");
     var mapper = nodetastic.CreateNodeTastic();
-    mapper.startServer(port);
-    done();
+    mapper.registerHandler("", {
+      hello: function(cb) {
+        cb(null, "hello world");
+      }
+    });
+    mapper.startServer(port, function() {
+      done();
+    });
   });
 
+  it('testing ping', function(done) {
+    httpHelper.createGet("/ping").getJson(function(err, result) {
+      assert(result.date);
+      done();
+    });
+  });
+
+  it('testing hello', function(done) {
+    httpHelper.createGet("/hello").getJson(function(err, result) {
+      console.log(result);
+      assert(result.data == "hello world");
+      done();
+    });
+  });
 });
 
