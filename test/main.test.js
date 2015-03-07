0a275af93ecd6a05b7b96b9d21be4db04cca85aa
Ralph Varjabedian <ralph.varjabedian@rakutenloyalty.com>
2015-03-07 20:38:43 +0200

    fix headers and changelog

--- test/main.test.js
+++ test/main.test.js
@@ -1,3 +1,9 @@
+/**
+ * Created by Ralph Varjabedian.
+ * nodetastic is licensed under the [BSD-3 License] http://bitbucket.com/ralphv/nodetastic/raw/master/LICENSE.
+ * do not remove this notice.
+ */
+
 'use strict';
 
 var should = require('should');
