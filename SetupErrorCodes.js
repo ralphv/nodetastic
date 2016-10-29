332d76e409bb45f155ef3e32c5d8d694db01e84a
Ralph Varjabedian <ralph.varjabedian@rakutenloyalty.com>
2016-10-29 21:17:58 +0300

    no http errors

--- SetupErrorCodes.js
+++ SetupErrorCodes.js
@@ -7,8 +7,8 @@
 var errors = require("cb-result").errors;
 
 /* code, enm, desc, regex */
-errors.add({code: 400, enum: "missingParameter", desc: "missing parameter", ext: {httpCode: 400}});
-errors.add({code: 401, enum: "invalidParameterType", desc: "invalid parameter type", ext: {httpCode: 400}});
-errors.add({code: 402, enum: "invalidState", desc: "invalid state for function", ext: {httpCode: 403}});
-errors.add({code: 403, enum: "notAuthorizedPath", desc: "unauthorized url/function", ext: {httpCode: 403}});
-errors.add({code: 404, enum: "invalidPath", desc: "invalid url/function", ext: {httpCode: 400}});
+errors.add({code: 400, enum: "missingParameter", desc: "missing parameter", ext: {httpCode: 200}});
+errors.add({code: 401, enum: "invalidParameterType", desc: "invalid parameter type", ext: {httpCode: 200}});
+errors.add({code: 402, enum: "invalidState", desc: "invalid state for function", ext: {httpCode: 200}});
+errors.add({code: 403, enum: "notAuthorizedPath", desc: "unauthorized url/function", ext: {httpCode: 200}});
+errors.add({code: 404, enum: "invalidPath", desc: "invalid url/function", ext: {httpCode: 200}});
